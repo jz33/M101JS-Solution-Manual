7bf3ca2119b87a1c2a457e0bf77867d0f33a5ecb
Jun <junzhengrice@gmail.com>
2015-08-30 19:28:19 -0500

    Create global_packages.sh

---
+++ Blog/global_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo npm install -g bcrypt-nodejs@~0.0.3
+sudo npm install -g body-parser@~1.13.3
+sudo npm install -g consolidate@~0.9.1
+sudo npm install -g cookie-parser@~1.3.5
+sudo npm install -g crypto@~0.0.3
+sudo npm install -g express@~4.x
+sudo npm install -g mongodb@~1.4.39
+sudo npm install -g swig@~0.14.0
+sudo npm install -g validator@~1.1.3
